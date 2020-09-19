30c12138117443365a297d4ea364da4e65bd783e
blankmaru <coopskywavy@gmail.com>
2020-09-19 20:05:10 +0300

    Start working on frontend, install packages, create navbar and file structure

---
+++ src/components/AppNavbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  Navbar,
+  Nav,
+  Container,
+  NavbarBrand
+} from 'react-bootstrap';
+
+const AppNavbar = props => {
+  return (
+    <>
+      <Navbar className="justify-content-between" bg="dark" variant="dark">
+        <Container>
+          <NavbarBrand>Book Library</NavbarBrand>
+          <Nav>
+              <Nav.Link href="/category">Categories</Nav.Link>
+              <Nav.Link href="/popular">Popular</Nav.Link>
+              <Nav.Link href="/books">Books</Nav.Link>
+          </Nav>
+          <Nav>
+              <Nav.Link href="/register">Register</Nav.Link>
+              <Nav.Link href="/login">Login</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default AppNavbar;
